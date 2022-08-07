4578cd784bc92a2ac8ca7795bb8e3417bcd7b3c5
GitHubMukeshFulwariya <33016542+GitHubMukeshFulwariya@users.noreply.github.com>
2022-08-07 17:11:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/googleprivacypolicy'));
+app.use(express.static(__dirname + '/dist/privacypolicy'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/googleprivacypolicy/index.html'));});
+'/dist/privacypolicy/index.html'));});
 app.listen(process.env.PORT || 8080);
