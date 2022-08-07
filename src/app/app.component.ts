a8fd22e79020215ee879a304de377073b50148c6
GitHubMukeshFulwariya <33016542+GitHubMukeshFulwariya@users.noreply.github.com>
2022-08-07 16:26:19 +0530

    Initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PrivacyPolicy';
+  mailTo : string = 'dineshphalwadiya1995@gmail.com';
+  linkedIn: string = 'https://www.linkedin.com/in/dinesh-falwadiya-8b3b84122';
+  youTube: string = 'https://www.youtube.com/'
+}
