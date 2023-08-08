9a117eef490ad96eb86cfb201d34ccf3796f811c
mukeshfulwariya <48671809+mukeshfulwariya@users.noreply.github.com>
2023-08-08 15:41:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/privacy-policy'));
 
-app.get('/*', function(req,res) {
+app.get('/policy', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/privacy-policy/index.html'));
 });
